421269bec187a48aeefeb0edf0a72c1913e83d15
Rizky Ramdhan Nugraha <rizkyramdhan.117h@gmail.com>
2022-03-17 22:53:33 +0700

    tes

---
+++ TP4/tpmodul4_1302203122/tpmodul4_1302203122/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302203122
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string namaKu;
+            Console.Write("Massukan Nama : ");
+            namaKu = Console.ReadLine();
+        }
+    }
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T nama)
+        {
+            Console.WriteLine("Halo user" + nama);
+        }
+    }
+}
